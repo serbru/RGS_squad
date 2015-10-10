e62d57d2b2db1c66fdf39c375da05675ac0e6699
Dylan Law <law.dylan.t@gmail.com>
2015-10-10 14:58:15 -0400

    Make wind blast move, add broken pusher script

---
+++ Assets/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent < Rigidbody2D >();
+        rb.velocity = new Vector2( speed, 0 );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Pusher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent< Rigidbody2D >();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D( Collision2D coll ) {
+        // FIXME Currently does nothing, as far as I can tell
+        // Apply push in the direction the object is currently traveling
+        coll.gameObject.GetComponent< Rigidbody2D >().AddForce( rb.velocity );
+    }
+}
